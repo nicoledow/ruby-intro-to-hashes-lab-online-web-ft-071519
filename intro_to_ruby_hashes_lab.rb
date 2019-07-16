fc2bcb37197e2eb387cd6405598b54f28ba16b39
Nicole Dow <nikki.dow71@gmail.com>
2019-07-16 16:57:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,7 +27,8 @@ def monopoly_with_third_tier
 	  :names => {
 	    :reading_railraod => {},
 	    :pennsylvania_railroad => {},
-	    :b_and_o_railroad => {}
+	    :b_and_o_railroad => {},
+	    :shortline => {}
 	  },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
